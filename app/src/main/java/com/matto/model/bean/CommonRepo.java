03a09fe0dd7dca11452bd3f4f5a9594c976548cb
zjlong <zhoujinlong@langtianhealth.com>
2016-04-27 17:13:22 +0800

    更换icon

--- app/src/main/java/com/matto/model/bean/CommonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.matto.model.bean;
-
-/**
- * author miekoz on 2016/3/21.
- * email  meikoz@126.com
- */
-public class CommonRepo {
-    public boolean error;
-}
--- app/src/main/java/com/matto/presenter/impl/LoginLogicImpl.java
+++ app/src/main/java/com/matto/presenter/impl/LoginLogicImpl.java
@@ -1,16 +1,9 @@
 package com.matto.presenter.impl;
 
-import android.util.Log;
-
 import com.matto.model.http.BaseHttpService;
 import com.matto.model.http.Factory;
-import com.matto.model.bean.Gank;
 import com.matto.presenter.LoginLogic;
 import com.matto.ui.view.LoginView;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * author meikoz on 2016/4/13.
