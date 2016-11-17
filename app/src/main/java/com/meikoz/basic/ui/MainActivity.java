f44369d7c663cc14112291a180f07d8e6c9ef15d
jinlong.zhou <jinlong.zhou@leediancn.com>
2016-11-17 14:15:57 +0800

    add custom style

--- app/src/main/java/com/meikoz/basic/ui/MainActivity.java
+++ app/src/main/java/com/meikoz/basic/ui/MainActivity.java
@@ -1,12 +1,15 @@
 package com.meikoz.basic.ui;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.meikoz.basic.R;
 import com.meikoz.basic.presenter.MainLogicI;
 import com.meikoz.core.base.BaseActivity;
+import com.meikoz.core.ui.CustomDialog;
 
-public class MainActivity extends BaseActivity implements MainLogicI.MainView{
+public class MainActivity extends BaseActivity implements MainLogicI.MainView {
 
     @Override
     protected int getLayoutResource() {
@@ -15,6 +18,18 @@ public class MainActivity extends BaseActivity implements MainLogicI.MainView{
 
     @Override
     protected void onInitView(Bundle bundle) {
+        new CustomDialog.Builder(this)
+                .setTitle(R.string.app_name)
+                .setMessage("您确定要删除dwadwa么?")
+                .setCancelable(false)
+                .setPositiveButton("ceshi", new CustomDialog.OnDialogClickListener() {
+                    @Override
+                    public void onClick(Dialog dialog, int which) {
+                        Toast.makeText(MainActivity.this, "确定操作" + which, Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 
     @Override
