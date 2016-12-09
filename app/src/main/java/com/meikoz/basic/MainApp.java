368b4e2eb284654b0c5b44ffac39b3780f74a3cf
jinlong.zhou <jinlong.zhou@leediancn.com>
2016-12-09 16:42:20 +0800

    add android crash

--- app/src/main/java/com/meikoz/basic/MainApp.java
+++ app/src/main/java/com/meikoz/basic/MainApp.java
@@ -3,6 +3,10 @@ package com.meikoz.basic;
 import com.meikoz.basic.app.Constants;
 import com.meikoz.core.MainApplication;
 import com.meikoz.core.api.RestApi;
+import com.meikoz.core.manage.crash.AndroidCrash;
+import com.meikoz.core.manage.interfacee.HttpReportCallback;
+
+import java.io.File;
 
 /**
  * @User: 蜡笔小新
@@ -15,5 +19,17 @@ public class MainApp extends MainApplication {
     public void onCreate() {
         RestApi.getInstance().bug(Constants.Config.DEVELOPER_MODE);
         super.onCreate();
+
+        configure();
+    }
+
+    private void configure() {
+        HttpReportCallback callback = new HttpReportCallback() {
+            @Override
+            public void uploadException2remote(File file) {
+                // param file is exception file
+            }
+        };
+        AndroidCrash.getInstance().setCrashReporter(callback).init(this);
     }
 }
--- core/src/main/java/com/meikoz/core/model/LogicProxy.java
+++ core/src/main/java/com/meikoz/core/model/LogicProxy.java
@@ -6,8 +6,6 @@ import com.meikoz.core.model.annotation.Implement;
 
 import java.lang.annotation.Annotation;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 public class LogicProxy {
