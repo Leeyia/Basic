8e3cb4db6d9e043b0b675fb0262b260046c203a0
zhoujinlong <zhoujinlong@dintech.com>
2018-10-27 18:46:24 +0800

    fix

--- things/http/src/main/java/com/racofix/develop/http/interceptor/SSLFactoryInterceptor.java
+++ things/http/src/main/java/com/racofix/develop/http/interceptor/SSLFactoryInterceptor.java
@@ -6,6 +6,7 @@ import okhttp3.Interceptor;
 import okhttp3.Response;
 
 public class SSLFactoryInterceptor implements Interceptor {
+
     @Override
     public Response intercept(Chain chain) throws IOException {
         return chain.proceed(chain.request());
--- things/support/src/main/java/com/racofix/develop/app/BaseActivity.java
+++ things/support/src/main/java/com/racofix/develop/app/BaseActivity.java
@@ -3,4 +3,6 @@ package com.racofix.develop.app;
 import android.support.v7.app.AppCompatActivity;
 
 public abstract class BaseActivity<T> extends AppCompatActivity {
+
+    protected abstract void init();
 }
