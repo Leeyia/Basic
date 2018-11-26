02caf7b7d7fd6cfe440695ba0deb029fedb0d235
zhoujinlong <zhoujinlong@dintech.com>
2018-11-26 11:08:21 +0800

    解决空指针问题

--- basics/mvp/src/main/java/com/racofix/basic/mvp/LogicViewModel.java
+++ basics/mvp/src/main/java/com/racofix/basic/mvp/LogicViewModel.java
@@ -2,28 +2,24 @@ package com.racofix.basic.mvp;
 
 import android.arch.lifecycle.ViewModel;
 
-import java.lang.ref.WeakReference;
-
 public final class LogicViewModel<T extends BaseLogic> extends ViewModel {
 
-    private WeakReference<T> mLogicWrf;
+    private T mLogicImpl;
 
     void setLogicImpl(T mLogicImpl) {
-        if (this.mLogicWrf == null || this.mLogicWrf.get() == null)
-            this.mLogicWrf = new WeakReference<>(mLogicImpl);
+        if (this.mLogicImpl == null)
+            this.mLogicImpl = mLogicImpl;
     }
 
-    T  getLogicImpl() {
-        return this.mLogicWrf.get();
+    T getLogicImpl() {
+        return this.mLogicImpl;
     }
 
     @Override
     protected void onCleared() {
         super.onCleared();
-        if (this.mLogicWrf != null) {
-            this.mLogicWrf.get().onLogicDestroy();
-            this.mLogicWrf.clear();
-            this.mLogicWrf = null;
+        if (this.mLogicImpl != null) {
+            this.mLogicImpl = null;
         }
     }
 }
