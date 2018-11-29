d4f0197e8344713d23dac90e618cb5e8d39fb8e8
zhoujinlong <zhoujinlong@dintech.com>
2018-11-29 10:42:39 +0800

    strategy

---
+++ basics/image/src/main/java/com/racofix/basic/image/Glide.java
@@ -0,0 +1,8 @@
+package com.racofix.basic.image;
+
+public class Glide {
+
+    public static Glide with(){
+        return new Glide();
+    }
+}
---
+++ basics/image/src/main/java/com/racofix/basic/image/Strategy.java
@@ -0,0 +1,4 @@
+package com.racofix.basic.image;
+
+public interface Strategy<T> {
+}
